5f3c461d1e8c18b7a4ddd7ef6136f06e8e31ad6e
metalzoa <metalzoa@naver.com>
2020-08-08 20:09:27 +0900

    aa

---
+++ src/main/java/IntegerNumbersStream.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class IntegerNumbersStream {
+
+    public static void main(String[] args){
+        final List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        final String result = numbers.stream().map(String::valueOf).collect(Collectors.joining((" : ")));
+
+
+
+        System.out.println(result);
+    }
+}
