2fd09c6f837d9f2160a517e7d4ec24fd27cafc9d
metalzoa <metalzoa@naver.com>
2020-08-08 20:25:46 +0900

    aa

--- src/main/java/IntegerNumbersStream.java
+++ src/main/java/IntegerNumbersStream.java
@@ -2,6 +2,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+/*
+결과값
+1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 10
+ */
 public class IntegerNumbersStream {
 
     public static void main(String[] args){
