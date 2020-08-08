d33854e3fbee1549c089ccd8629ec83a2159af52
metalzoa <metalzoa@naver.com>
2020-08-08 20:31:01 +0900

    aa

---
+++ src/main/java/StringNumbersStream.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/*
+결과값
+1 : 2 : 3 : 4 : 5 : 6 : 7 : 8 : 9 : 10
+ */
+public class StringNumbersStream {
+
+    public static void main(String[] args){
+        final List<String> numbers = Arrays.asList("1","2","3","4","5","6","7","8","9","10");
+
+        final String result = numbers
+                .stream()
+                .collect(Collectors.joining((" : ")));
+        System.out.println(result);
+    }
+}
