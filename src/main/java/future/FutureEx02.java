87720ca0f2133130c705010273877e31c893b6e8
metalzoa <metalzoa@naver.com>
2020-08-20 00:22:26 +0900

    executor sample

--- src/main/java/future/FutureEx02.java
+++ src/main/java/future/FutureEx02.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-
+/* 한글 */
 public class FutureEx02 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService es = Executors.newCachedThreadPool();
